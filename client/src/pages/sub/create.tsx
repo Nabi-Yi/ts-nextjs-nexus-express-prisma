e28bf5386737376831888a33070fc3d072c05a17
Nabi-Yi <nabiyi926@gmail.com>
2023-01-17 15:33:55 +0900

    <client> typo 수정

--- client/src/pages/sub/create.tsx
+++ client/src/pages/sub/create.tsx
@@ -15,8 +15,8 @@ export default function create() {
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/subs", {title,description});
-      router.push(`/r/${res.data.name}`);
+      const res = await axios.post("/sub", {title,description});
+      router.push(`/r/${res.data.title}`);
     } catch (error: any) {
       console.log(error)
       setErrors(error?.response?.data || {});
@@ -28,7 +28,7 @@ export default function create() {
         <div className="w-10/12 mx-auto md:w-96">
           <h1 className="mb-2 text-lg font-medium">커뮤니티 만들기</h1>
           <hr />
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="my-6">
               <p className="font-medium"> Title </p>
               <p className="mb-2 text-xs text-gray-400">
