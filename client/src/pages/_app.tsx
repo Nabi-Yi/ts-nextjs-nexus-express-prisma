b3cfc68226d749abcd785500617272d30f2f2f77
Nabi-Yi <nabiyi926@gmail.com>
2023-01-17 15:33:19 +0900

    <client> 로그인 시(Authorization 쿠키 있을 시) 토큰header에 default로 설정

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,9 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Axios from "axios";
 import {AuthProvider} from "../context/auth";
+import Cookies from "js-cookie";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const accessToken = Cookies.get("Authorization");
+  console.log(accessToken);
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
+  if(accessToken) Axios.defaults.headers["Authorization"] = `Bearer ${accessToken}`;
+
   return <AuthProvider>
     <Component {...pageProps} />;
   </AuthProvider>
